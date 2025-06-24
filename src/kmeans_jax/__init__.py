75cbc704f39ecbd6e8727ba4a249a4eec0551612
DSilva27 <david.silva@yale.edu>
2025-06-24 13:40:00 -0400

    implement hartigan

--- src/kmeans_jax/__init__.py
+++ src/kmeans_jax/__init__.py
@@ -3,7 +3,7 @@ from . import (
     kmeans_in_practice as kmeans_in_practice,
     theory_experiments as theory_experiments,
 )
-from .__about__ import __version__
+from .kmeansjax_version import __version__
 
 
 __all__ = ["__version__"]
